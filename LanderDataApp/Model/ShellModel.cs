5dbd245a18a78c74b64c53377208321e2bcaf4c9
Austin Briere <austinbriere93@gmail.com>
2023-12-21 13:56:05 -0500

    Add project files.

---
+++ LanderDataApp/Model/ShellModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanderDataApp.Model
+{
+    public class ShellModel //: INotifyPropertyChanged
+    {
+        public string? HelloString
+        {
+            get;
+            set;
+        }
+
+
+        public ShellModel()
+        {
+            
+        }
+
+        //public event PropertyChangedEventHandler? PropertyChanged;
+
+        //public List<string> ImportDefaultValues()
+        //{
+
+        //}
+    }
+}
---
+++ LanderDataApp/ViewModel/ShellViewModel.cs
@@ -0,0 +1,38 @@
+﻿using LanderDataApp.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanderDataApp.ViewModel
+{
+    public class ShellViewModel : INotifyPropertyChanged
+    {
+        public string? HelloString
+        {
+            get;
+            set;
+        }
+
+        private ShellModel? _shellModel;
+        public ShellModel? ShellModel
+        {
+            get => _shellModel;
+            set
+            {
+                _shellModel = value;
+
+            }
+        }
+
+        public ShellViewModel()
+        {
+            HelloString = "Hello";
+            ShellModel = new ShellModel();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
